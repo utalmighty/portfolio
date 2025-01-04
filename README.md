38bd001de34e943bd44fbe3d5497a1fe3b018aad
utalmighty <utalmighty@gmail.com>
2025-01-04 19:17:46 +0530

    Added TODO

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Portfolio
+Visit: https://utalmighty.github.io/portfolio/
+
+TODO:
+- [x]: Create React App
+- [x]: Profile links
+- [x]: Github pages
+- [ ]: Experience Timeline
+- [ ]: Skill section
+- [ ]: Download Resume section
+- [ ]: Contact me
+- [ ]: Live github stats
+- [ ]: Live leetcode stats
+- [ ]: Auto Dark/Light Mode
+- [ ]: Acralic Navbar
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,8 +1,23 @@
 import './index.css';
 
 function Profile() {
+
+    const profiles = [
+        { page: "Github", link: "https://www.github.com/utalmighty"},
+        { page: "Leetcode", link: "https://www.leetcode.com/u/utalmighty"},
+    ];
+
+    const listItems = profiles.map(profile=>
+        <li key={profile.page}>
+            <a href={profile.link} target='blank'>{profile.page}</a>
+        </li>
+    );
+
     // JSX: Javascript XML
-    return <h1 className="font-serif">Utkarsh</h1>;
+    return (<>
+        <h1 className="align-middle">Utkarsh</h1>
+        <ul>{listItems}</ul>
+    </>);
 }
 
 export default Profile;
