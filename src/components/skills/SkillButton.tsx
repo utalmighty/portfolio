64182f36fe0b495325dc9f12c207ef9b2473fd5a
utalmighty <utalmighty@gmail.com>
2025-01-19 23:25:12 +0530

    Added Skills section

--- src/components/skills/SkillButton.tsx
+++ src/components/skills/SkillButton.tsx
@@ -6,22 +6,28 @@ interface Props {
 }
 
 export default function SkillButton(skill: Props) {
-  const about = skill.about && <h6 className="text-xs font-light text-slate-400">{skill.about}</h6>
-  
+  const about = skill.about && (
+    <h6 className="text-xs font-light text-slate-400">{skill.about}</h6>
+  );
+
   return (
     <>
-      <div className="ml-5 mr-5 mt-10">
-        <a id={skill.name}>
-          <div>
-            <div className="padding p-2 flex h-full justify-evenly">
-              <img src={skill.icon} className="w-10 sm:pr-2"></img>
-              <div className="flex flex-col justify-center">
-                <h1 className="font-semibold text-sm text-slate-950">{skill.name}</h1>
-                {about}
+      <div className="w-1/2 md:w-1/3 m-auto flex justify-center">
+        <div className="m-5">
+          <a id={skill.name}>
+            <div>
+              <div className="padding p-2 flex h-full">
+                <img src={skill.icon} className="w-10 sm:pr-2"></img>
+                <div className="flex flex-col justify-center">
+                  <h1 className="font-semibold text-sm text-slate-950">
+                    {skill.name}
+                  </h1>
+                  {about}
+                </div>
               </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
     </>
   );
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,33 +1,38 @@
 import SkillButton from "./SkillButton";
+import java from "../../assets/java.svg";
+import spring from "../../assets/spring.svg";
+import react from "../../assets/react.svg";
+import python from "../../assets/python.svg";
+import postgre from "../../assets/postgre.svg";
 
 export default function Skills() {
   const skills = [
     {
-      icon: "Java",
+      icon: java,
       name: "Java",
       about: "Programming Language",
       yoe: 3,
     },
     {
-      icon: "SpringBoot",
+      icon: spring,
       name: "SpringBoot",
       about: "REST-API Framework",
       yoe: 3,
     },
     {
-      icon: "React",
+      icon: react,
       name: "React",
       about: "Frontend Library",
       yoe: 1,
     },
     {
-      icon: "Python",
+      icon: python,
       name: "Python",
       about: "Programming Language",
       yoe: 2,
     },
     {
-      icon: "PostgreSQL",
+      icon: postgre,
       name: "PostgreSQL",
       about: "Relational Database",
       yoe: 2,
@@ -36,20 +41,23 @@ export default function Skills() {
 
   const skillItems = skills.map((skill) => (
     <SkillButton
-      name={skill.icon}
-      icon={skill.name}
+      name={skill.name}
+      icon={skill.icon}
       about={skill.about}
       yoe={skill.yoe}
     />
   ));
 
   return (
-      <>
-        <div className="flex justify-center">
-          <div className="flex flex-col sm:flex-row justify-center">
+    <>  
+      <p className="text-center text-3xl sm:text-4xl font-semibold mt-32 mb-10">SKILLS</p>
+        <div className="flex mb-3">
+          <div className="w-0 md:w-1/6"></div>
+          <div className="w-full md:w-4/6 flex md:flex-row flex-wrap">
             {skillItems}
           </div>
+          <div className="w-0 md:w-1/6"></div>
         </div>
-      </>
-    );
+    </>
+  );
 }
