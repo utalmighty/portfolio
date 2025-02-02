b3701e576331099515dfb6a9bf5dd77fb8847d2d
utalmighty <utalmighty@gmail.com>
2025-02-02 17:35:24 +0530

    Added more details in careers

--- src/components/career/Institute.tsx
+++ src/components/career/Institute.tsx
@@ -2,18 +2,22 @@ import { TimelineProp } from "./Tab";
 
 export default function Institute(item: TimelineProp) {
 
-    const points = item.about.map(point=> (
-        <li className="text-sm">{point}</li>
+    const points = item.about.map((point, indx)=> (
+        <li key={indx} className="text-lg md:text-sm">{point}</li>
     ));
+
+    const position = item.position ? item.position + " | " + item.location : item.location;
 
   return (
     <>
-    <div className="flex flex-row">
-        <div>{item.icon}</div>
-        <div className="ml-5">
-            <div className="text-xs text-gray-500">{item.date}</div>
-            <div className="font-semibold leading-none">{item.name}</div>
-            <p className="text-sm text-gray-500">{item.position}</p> 
+    <div className="flex flex-row mt-5 mb-10">
+        <div className="mt-2">
+          <img src={item.icon} className="m-1 size-14 border border-gray-400 rounded-full object-contain"></img>
+        </div>
+        <div className="ml-10">
+            <div className="text-sm text-gray-500 mb-1 md:mb-0">{item.date}</div>
+            <div className="text-xl md:text-lg font-semibold leading-none mb-1 md:mb-0">{item.name}</div>
+            <p className="text-md md:text-sm text-gray-500 mb-1">{position}</p>
             <div>{points}</div>
         </div>
       </div>
--- src/components/career/Tab.tsx
+++ src/components/career/Tab.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import Institute from "./Institute";
+import infosys from "../../assets/infosys.jpg";
+import cognizant from "../../assets/cognizant.png";
+import aktu from "../../assets/aktu.png";
+import cms from "../../assets/cms.png"
 
 export interface TimelineProp {
   icon: string;
@@ -7,68 +11,107 @@ export interface TimelineProp {
   about: string[];
   date: string;
   position: string;
+  location: string;
   link: string;
+  specialities: SpecialityProp[]
+}
+
+interface SpecialityProp {
+  name: string;
+  icon: string;
 }
 
 export default function Tab() {
 
   const educationTimelineData : TimelineProp[] = [
       {
-        icon: "edu1 icon",
-        name: "edu1 name",
+        icon: aktu,
+        name: "Dr. A.P.J Abdul Kalam Technical University",
         about: ["point1 edu1 message", "point2 edu1 message"],
-        date: "edu1 date",
-        position: "edu1 pos",
-        link: "edu1 link"
+        date: "Aug 2017 - Apr 2021",
+        position: "Bachelor of Technology (Information Technology)",
+        link: "edu1 link",
+        location: "Lucknow, Uttar Pradesh",
+        specialities: []
       },
       {
-        icon: "edu2 icon",
-        name: "edu2 name",
+        icon: cms,
+        name: "City Montessori School",
         about: ["point1 edu2 message", "point2 edu2 message"],
         date: "edu2 date",
-        position: "edu2 pos",
-        link: "edu2 link"
+        position: "",
+        link: "edu2 link",
+        location: "Lucknow, Uttar Pradesh",
+        specialities: []
       }];
 
     const experienceTimelineData : TimelineProp[] = [
       {
-        icon: "exp1 icon",
-        name: "exp1 name",
-        about: ["point1 exp1 message", "point2 exp1 message"],
-        date: "exp1 date",
-        position: "exp1 pos",
-        link: "exp1 link"
+        icon: infosys,
+        name: "Infosys",
+        about: ["Full stack Developer working with Apple's Enginneering Team", "point2 exp1 message"],
+        date: "Jan 2024 - Present",
+        position: "Specialist Programmer",
+        link: "exp1 link",
+        location: "Pune, Maharashtra",
+        specialities: []
       },
       {
-        icon: "exp2 icon",
-        name: "exp2 name",
+        icon: infosys,
+        name: "Infosys",
         about: ["point1 exp2 message", "point2 exp2 message"],
-        date: "exp2 date",
-        position: "exp2 pos",
-        link: "exp2 link"
+        date: "Apr 2022 - Jan 2024",
+        position: "Digital Specialist Engineer",
+        link: "exp2 link",
+        location: "Pune, Maharashtra",
+        specialities: []
+      },
+      {
+        icon: cognizant,
+        name: "Cognizant",
+        about: ["Backend Developer", "point2 exp1 message"],
+        date: "Jan 2024 - Present",
+        position: "Programmer Analyst",
+        link: "exp1 link",
+        location: "Remote",
+        specialities: []
+      },
+      {
+        icon: cognizant,
+        name: "Cognizant",
+        about: ["point1 exp2 message", "point2 exp2 message"],
+        date: "Apr 2022 - Jan 2024",
+        position: "Intern",
+        link: "exp2 link",
+        location: "Remote",
+        specialities: []
       }];
 
       const educationTimeline = educationTimelineData.map((item)=> (
         <Institute 
-          key={item.name}
+          key={item.name + item.date}
           icon={item.icon}
           name={item.name}
           about={item.about}
           date={item.date}
           position={item.position}
           link={item.link}
+          location={item.location}
+          specialities={item.specialities}
         />
       ));
 
       const experienceTimeline = experienceTimelineData.map((item)=> (
         <Institute 
-          key={item.name}
+          key={item.name + item.date}
           icon={item.icon}
           name={item.name}
           about={item.about}
           date={item.date}
           position={item.position}
           link={item.link}
+          location={item.location}
+          specialities={item.specialities}
         />
       ));
 
@@ -94,7 +137,7 @@ export default function Tab() {
         <div className={`w-1/2 text-center rounded-md ${state == "Education" ? "bg-slate-400 shadow-sm  shadow-slate-400" : "hover:bg-slate-200"}`} onClick={()=> handleClick("Education")}>Education</div>
       </div>
       <div className="w-full m-3 p-1 border border-gray-300 rounded-lg bg-slate-50 shadow-sm"> {/* 👈🏼 Make this scrollable */}
-        <div>
+        <div className="ml-10 sm:ml-20">
           {timeline}
         </div>
       </div>
