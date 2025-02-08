bcdd3ee1f3b43c57b6f6ec444e4a3ec11d39aa4c
utalmighty <utalmighty@gmail.com>
2025-02-08 20:56:09 +0530

    Added Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Career from "./components/career/Career";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
+import Project from "./components/project/Project";
 import Skills from "./components/skills/Skills";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Profile />
       <Skills />
       <Career />
+      <Project />
     </div>
   );
 }
