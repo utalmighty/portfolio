64675a617326ae89845c3a80e3d8bba638d5ed57
utalmighty <utalmighty@gmail.com>
2025-04-05 21:28:38 +0530

    Some more progress

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,43 +1,153 @@
-import ProjectTile, { ProjectProp } from "./ProjectTile";
+import github from "../../assets/project/icons/github.svg";
+import website from "../../assets/project/icons/website.svg";
+import algoviz from "../../assets/project/screenshots/AlgoViz.png";
 import Wellwisher from "../../assets/project/screenshots/Wellwisher.png";
-import github from "../../assets/project/icons/github.svg"
+import css from "../../assets/technologies/css.svg";
+import docker from "../../assets/technologies/docker.svg";
+import html from "../../assets/technologies/html.svg";
+import java from "../../assets/technologies/java.svg";
+import js from "../../assets/technologies/js.svg";
+import mongodb from "../../assets/technologies/mongodb.svg";
+import node from "../../assets/technologies/nodejs.svg";
+import postgre from "../../assets/technologies/postgre.svg";
+import rabbitmq from "../../assets/technologies/rabbitmq.svg";
+import reactor from "../../assets/technologies/reactor.png";
+import spring from "../../assets/technologies/spring.svg";
+import ProjectTile, { ProjectProp } from "./ProjectTile";
 
 export default function Project() {
-
-    const projectsData: ProjectProp[] = [
+  const projectsData: ProjectProp[] = [
+    {
+      name: "Well Wisher",
+      icon: Wellwisher,
+      about: [
+        "Well-Wisher, wishes your loved ones on their special occasion via email.",
+      ],
+      date: "date",
+      links: [
         {
-            name: "Well Wisher",
-            icon: Wellwisher,
-            about: ["about"],
-            date: "date",
-            links: [{link: "https://www.google.com", about: "Must be link", icon: github},
-                    {link: "https://www.google.com", about: "Must be link2", icon: "icon"}],
-            technologies: [ {name: "tech1", icon: "icon1"}, 
-                            {name: "tech2", icon: "icon2"},
-                            {name: "tech3", icon: "icon1"},
-                            {name: "tech4", icon: "icon1"},
-                            {name: "tech5", icon: "icon1"},
-                            {name: "tech6", icon: "icon1"}]
-        }
-    ];
+          link: "https://github.com/Shashank2707/Well-wisher",
+          about: "Github",
+          icon: github,
+        },
+        {
+          link: "https://utalmighty.github.io/Hudukian/",
+          about: "Website",
+          icon: website,
+        },
+      ],
+      technologies: [
+        { name: "Java", icon: java },
+        { name: "Spring", icon: spring },
+        { name: "PostgreSQL", icon: postgre },
+        { name: "RabbitMQ", icon: rabbitmq },
+        { name: "HTML", icon: html },
+        { name: "CSS", icon: css },
+        { name: "JavaScript", icon: js },
+      ],
+    },
+    {
+      name: "Huduk SOS",
+      icon: Wellwisher,
+      about: ["Self-hosted, non-blocking Simple Object Storage."],
+      date: "date",
+      links: [
+        {
+          link: "https://github.com/utalmighty/Huduk-SOS",
+          about: "Github",
+          icon: github,
+        },
+      ],
+      technologies: [
+        { name: "Java", icon: java },
+        { name: "MongoDB ", icon: mongodb },
+        { name: "Reactor Framework", icon: reactor },
+      ],
+    },
+    // {
+    //   name: "P2PFS",
+    //   icon: Wellwisher,
+    //   about: ["Peer to Peer file sharing without size limit."],
+    //   date: "date",
+    //   links: [
+    //     {
+    //       link: "https://github.com/utalmighty/P2PFS",
+    //       about: "Github",
+    //       icon: github,
+    //     },
+    //     {
+    //       link: "https://p2pfs-utalmighty.b4a.run/",
+    //       about: "Website",
+    //       icon: website,
+    //     },
+    //   ],
+    //   technologies: [
+    //     { name: "Java", icon: java },
+    //     { name: "JavaScript", icon: js },
+    //     { name: "Docker", icon: docker },
+    //     { name: "HTML", icon: html },
+    //     { name: "CSS", icon: css },
+    //   ],
+    // },
+    // {
+    //   name: "AC-JS",
+    //   icon: Wellwisher,
+    //   about: [
+    //     "Command line tool with powerful unix-like cat commands written in JavaScript.",
+    //   ],
+    //   date: "date",
+    //   links: [
+    //     {
+    //       link: "https://github.com/utalmighty/AC-JS",
+    //       about: "Github",
+    //       icon: github,
+    //     },
+    //   ],
+    //   technologies: [
+    //     { name: "Node", icon: node },
+    //     { name: "JS", icon: js },
+    //   ],
+    // },
+    // {
+    //   name: "Algo Viz",
+    //   icon: algoviz,
+    //   about: ["Algorithm visualization web-based interactive platform."],
+    //   date: "date",
+    //   links: [
+    //     {
+    //       link: "https://github.com/utalmighty/ALGOVIZ",
+    //       about: "Github",
+    //       icon: github,
+    //     },
+    //   ],
+    //   technologies: [
+    //     { name: "HTML", icon: html },
+    //     { name: "JavaScript", icon: js },
+    //     { name: "CSS", icon: css },
+    //   ],
+    // }
+  ];
 
-    const projects = projectsData.map((project)=> (
-        <ProjectTile 
-            key={project.name + project.date}
-            icon={project.icon} 
-            name={project.name} 
-            about={project.about}
-            date={project.date}
-            links={project.links} 
-            technologies={project.technologies}/>
-    ));
+  const projects = projectsData.map((project) => (
+    <ProjectTile
+      key={project.name + project.date}
+      icon={project.icon}
+      name={project.name}
+      about={project.about}
+      date={project.date}
+      links={project.links}
+      technologies={project.technologies}
+    />
+  ));
 
   return (
     <>
       <p className="text-center text-3xl sm:text-4xl font-semibold mt-32 mb-10">
         PROJECTS
       </p>
-      <div>{projects}</div>
+      <div className="flex flex-wrap md:flex-row justify-center">
+        {projects}
+      </div>
     </>
   );
 }
